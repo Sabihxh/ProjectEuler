698ca18b1f06ed65e6d6e2f8e7982ef4299c5350
Sabih <sabihhasan12@hotmail.co.uk>
2019-05-27 11:17:26 +0100

    test

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sabihhasan/Desktop/projects/ProjectEuler/venv"
+set -gx VIRTUAL_ENV "/Users/sabihhasan/Desktop/projects/projecteuler/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
